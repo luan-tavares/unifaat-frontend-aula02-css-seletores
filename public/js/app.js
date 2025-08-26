647df01b5de40c2ac899e87c7d7469fe66a88ea0
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-08-26 12:09:57 -0300

    refresh

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+const controlesElement = document.querySelector("aside .controles");
+
+const inputNomeElement = document.querySelector("aside .controles input[type='text']");
+
+const selectTipoElement = controlesElement.querySelector("select");
+
+console.log(
+    controlesElement,
+    inputNomeElement,
+    selectTipoElement
+);
